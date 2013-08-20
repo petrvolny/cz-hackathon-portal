f212c05782813a480ef692e9a82094f43aec5924
Petr Volny <petr.volny@gooddata.com>
2013-08-20 11:50:41 +0200

    Robots.txt

--- web.js
+++ web.js
@@ -6,6 +6,9 @@ app.use(express.static(__dirname + '/'));
 app.get('/', function(request, response) {
   response.render('index.html');
 });
+app.get('/robots.txt', function(request, response) {
+  response.render('robots.txt');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
