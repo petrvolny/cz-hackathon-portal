6536e2b0b477b12d0ad7d6c8d6f980171409e3ad
Petr Volny <petr.volny@gooddata.com>
2013-08-20 11:48:03 +0200

    Node.js skeleton

---
+++ web.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.use(express.static(__dirname + '/'));
+app.get('/', function(request, response) {
+  response.render('index.html');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
