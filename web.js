264e072b56465316ae984012d83171a603e6fa11
Petr Volny <petr.volny@gooddata.com>
2013-08-21 21:17:34 +0200

    Web.js points to dist

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
-app.use(express.static(__dirname+'/app'));
+app.use(express.static(__dirname+'/dist'));
 
 // app.post('/subscribe', mailchimp.addSubscriber);
 
